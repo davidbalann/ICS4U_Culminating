880a6f96695407119b5225df1f4275ca640a2dd4
David Balan <david.balan2@student.tdsb.on.ca>
2023-01-23 01:14:45 -0500

    scroll bar added(but not in right format), sorting done needs implementation in other pages add part that collects ingrediants puts them in created text file and tells windows to open that file fix overall gui

--- untitled/src/viewPage.java
+++ untitled/src/viewPage.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
 public class viewPage extends JFrame {
@@ -27,11 +28,6 @@ public class viewPage extends JFrame {
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scroll.setViewportView(viewArea);
         setVisible(true);
-
-
-
-
-
 
         for (int i = 0; i < Main.recipeList.size(); ++i)
             viewArea.append(Main.recipeList.get(i).formattedToString());
